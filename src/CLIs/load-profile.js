e52b77ceb0f031fc5cd6c965f4c53617dc91d6b1
Shao, Zhimin <zhimin.shao@sap.com>
2023-08-19 12:09:42 +0800

    update

--- src/CLIs/load-profile.js
+++ src/CLIs/load-profile.js
@@ -1,9 +1,10 @@
 const {
   loadProfileGzipped,
-  connect,
-  login,
-  client,
 } = require('../commands');
+
+const {
+  basic,
+} = require('./basic');
 
 const commandOptions = {
   dashId: {
@@ -14,38 +15,11 @@ const commandOptions = {
   }
 }
 
-const command = (options) => {
-  console.debug(options);
-  const {
-    username,
-    password,
-    host,
-    port,
-    appname,
-  } = process.env;
-  if (username && password && host && port) {
-    console.debug(username, password, host, port, appname);
-    const blynk = client(host, port);
-    const loginCallback = (username, password, appName) => {
-      login.commandOnly(blynk, username, password);
-    };
-    connect(blynk, loginCallback, username, password, appname)
-    .then((status) => {
-      return loadProfileGzipped.command(blynk, options);	
-    })
-    .then((data) => {
-      console.log(JSON.stringify(JSON.parse(data), null, 2));
-    })
-    .catch((error) => {
-      console.error(error);
-    })
-    .finally(() => {
-      if (blynk && blynk.socket) {
-        blynk.socket.destroy();
-      }
-    });
-  }
-};
+const callbackCommand = (blynk, options) => (status) => loadProfileGzipped.command(blynk, options);
+const callbackThen = () => (data) => {
+  console.log(JSON.stringify(JSON.parse(data), null, 2));
+}
+const command = basic(callbackCommand, callbackThen);
 
 const exportFunctions = {
   commandOptions,
