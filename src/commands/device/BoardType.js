368eb2dee7c0e19116476d72ca9999ca3dbe5994
Shao, Zhimin <zhimin.shao@sap.com>
2023-08-21 17:13:51 +0800

    update

--- src/commands/device/BoardType.js
+++ src/commands/device/BoardType.js
@@ -1,6 +1,8 @@
 const BoardType = {
   ESP8266      		    : 'ESP8288',
   NodeMCU      		    : 'NodeMCU',
+  RaspberryPi3B       : 'Raspberry Pi 3 B',
+  RaspberryPi2        : 'Raspberry Pi 2/A+/B+',
 }
 
 const exportFunctions = {
