115666c1e00b7e6c028a8615a8b243d0c36a9ceb
Shao, Zhimin <zhimin.shao@sap.com>
2023-08-28 21:04:14 +0800

    update

--- src/commands/send.js
+++ src/commands/send.js
@@ -1,26 +1,56 @@
 const SEND_TIMEOUT = 10000;
 
+// const buildBlynkMessage = (cmd, msgId, cmdBody, isAppCommand) => {
+// 	const BLYNK_HEADER_SIZE = 7;
+// 	const bodyLength = (cmdBody ? cmdBody.length : 0);
+// 	console.log('body length: ', bodyLength);
+
+// 	const bufArray = new ArrayBuffer(BLYNK_HEADER_SIZE + bodyLength);
+// 	const dataview = new DataView(bufArray);
+// 	dataview.setUint8(0, cmd);
+// 	dataview.setUint16(1, msgId);
+// 	dataview.setUint32(3, bodyLength);
+
+// 	if (bodyLength > 0) {
+// 		//todo optimize. should be better way
+// 		const buf = new ArrayBuffer(bodyLength); // 2 bytes for each char
+// 		const bufView = new Uint8Array(buf);
+// 		for (let i = 0, offset =  7; i < cmdBody.length; i++, offset += 1) {
+// 			dataview.setInt8(offset, cmdBody.charCodeAt(i));
+// 		}
+// 	}
+// 	return new Buffer(bufArray);
+// }
+
 const buildBlynkMessage = (cmd, msgId, cmdBody, isAppCommand) => {
-	const BLYNK_HEADER_SIZE = 7;
+	const BLYNK_HEADER_SIZE = isAppCommand ? 7 : 5;
 	const bodyLength = (cmdBody ? cmdBody.length : 0);
 	console.log('body length: ', bodyLength);
 
 	const bufArray = new ArrayBuffer(BLYNK_HEADER_SIZE + bodyLength);
+	let offset;
 	const dataview = new DataView(bufArray);
 	dataview.setUint8(0, cmd);
 	dataview.setUint16(1, msgId);
-	dataview.setUint32(3, bodyLength);
+	if (isAppCommand) {
+		dataview.setUint32(3, bodyLength);
+		offset = 7;
+	} else {
+		dataview.setUint16(3, bodyLength);
+		offset = 5;
+	}
 
 	if (bodyLength > 0) {
 		//todo optimize. should be better way
 		const buf = new ArrayBuffer(bodyLength); // 2 bytes for each char
 		const bufView = new Uint8Array(buf);
-		for (let i = 0, offset =  7; i < cmdBody.length; i++, offset += 1) {
+		for (let i = 0; i < cmdBody.length; i++, offset += 1) {
 			dataview.setInt8(offset, cmdBody.charCodeAt(i));
 		}
 	}
 	return new Buffer(bufArray);
 }
+
 
 const createMessage = (commandAndBody, code, client, isAppCommand, joinToken) => {
 	let cmdBody = null;
