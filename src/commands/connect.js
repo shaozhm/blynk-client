94feea680cfe2fb028381a11b0124008432c1a78
Shao, Zhimin <zhimin.shao@sap.com>
2023-10-02 18:19:53 +0800

    update

--- src/commands/connect.js
+++ src/commands/connect.js
@@ -82,7 +82,7 @@ const connect = (client, callback, isTLS, ...authOptions) => {
     });
     
 		let socket;
-		if (isTLS === 'yes' || isTLS === 'true') {
+		if (typeof(isTLS) === 'boolean' && isTLS || typeof(isTLS) === 'string' && (isTLS === 'yes' || isTLS === 'true')) {
 			socket = tls.connect(port, host, client.options, () => {
 				callback(...authOptions);
     });
