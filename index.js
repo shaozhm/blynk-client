6762e1f470986878f54792de4bff90f3cdc8dcdb
Shao, Zhimin <zhimin.shao@sap.com>
2023-10-18 16:40:46 +0800

    update

---
+++ index.js
@@ -0,0 +1,76 @@
+const {
+  createProject,
+  deleteProject,
+  updateProjectSettings,
+} = require('./src/commands/dashboard');
+
+const {
+  BoardType,
+  ConnectionType,
+  createDevice,
+  deleteDevice,
+  getDevice,
+  updateDevice,
+  getDevices,
+} = require('./src/commands/device');
+
+const {
+  loadProfileGzipped,
+  activateDashboard,
+  deactivateDashboard,
+  getEnergy,
+  assignToken,
+  getProjectByToken,
+  refreshToken,
+  getCloneCode,
+  getProjectByClonedToken,
+  hardware,
+} = require('./src/commands/main');
+
+const {
+  createWidget,
+  deleteWidget,
+  getWidget,
+} = require('./src/commands/widget');
+
+const {
+  getShareToken,
+  refreshShareToken,
+  sharing,
+} = require('./src/commands/sharing');
+
+const exportFunctions = {
+  //dashboard
+  createProject,
+  deleteProject,
+  updateProjectSettings,
+  //device
+  BoardType,
+  ConnectionType,
+  createDevice,
+  deleteDevice,
+  getDevice,
+  updateDevice,
+  getDevices,
+  //main
+  loadProfileGzipped,
+  activateDashboard,
+  deactivateDashboard,
+  getEnergy,
+  assignToken,
+  getProjectByToken,
+  refreshToken,
+  getCloneCode,
+  getProjectByClonedToken,
+  hardware,
+  //widget
+  createWidget,
+  deleteWidget,
+  getWidget,
+  //sharing
+  getShareToken,
+  refreshShareToken,
+  sharing,
+};
+
+module.exports = exportFunctions;
